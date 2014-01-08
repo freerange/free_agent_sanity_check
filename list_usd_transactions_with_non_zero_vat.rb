f537f5047e1cb73a1ea39442ce0fcde037b81d2d
James Mead <james@floehopper.org>
2014-01-08 19:41:15 +0000

    Exclude some specific transactions which accidentally match.
    We have two transactions relating to [1] which match the new regular
    expression, but which are not actually foreign currency transactions.
    
    This is obviously a bit of a hack, but it'll do for now.
    
    [1] Eventbrite - Ticket to First International Software Archeology Conference

--- list_usd_transactions_with_non_zero_vat.rb
+++ list_usd_transactions_with_non_zero_vat.rb
@@ -10,7 +10,7 @@ FreeRange.bank_accounts.each do |account|
   FreeRange.bank_transactions(account.id, :from => Date.parse("2010-10-31"), :to => Date.today).each do |transaction|
     if %r{USD|\$|INT'L}.match(transaction.name)
       transaction.bank_account_entries.each do |entry|
-        if entry.sales_tax_rate > 0
+        if (entry.sales_tax_rate > 0) && (!%r{FIRST INTERNAT 8888102063}.match(transaction.name))
           successful = false
           puts [account.name, transaction.dated_on.to_date, transaction.name, entry.sales_tax_rate.to_f].join("\t")
         end
